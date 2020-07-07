21be01ffdd3439392b8e5ee9ea907ad17e260af3
Anusha-123-Creator <64792158+Anusha-123-Creator@users.noreply.github.com>
2020-07-07 16:32:40 +0530

    minute.java is created

---
+++ minute.java
@@ -0,0 +1,30 @@
+package minute;
+import java.util.Scanner;
+
+/**
+ *
+ * @author anusha
+ */
+public class Minute {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    
+        // TODO code application logic here
+    }
+    
+}
